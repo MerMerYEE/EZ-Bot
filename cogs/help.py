91ea7cd34f009cc1b9a7ed7e44d7eb66618c8292
SaltCube <shio7113@gmail.com>
2020-05-26 16:13:22 +0900

    someone need to add page system

--- cogs/help.py
+++ cogs/help.py
@@ -13,6 +13,7 @@ class Help(commands.Cog):
         await ctx.trigger_typing()
         embed = discord.Embed(title="도움말", description= "**EZ봇 1.0 \n Prefix: ``.``**", color=0xffffff)
         embed.add_field(name='**도움**', value='**이 메시지를 보여줍니다.**', inline = False)
+        embed.add_field(name='**아바타**', value='**본인의 아바타 또는 멘션된 유저의 아바타를 보여줍니다.**', inline = False)
         embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio.png")
         await ctx.send(embed=embed)
 
