e83dc162089dd168ae8711bb38f92b63687caafa
SaltCube <shio7113@gmail.com>
2020-05-26 10:25:28 +0900

    c5

---
+++ cogs/translate.py
@@ -0,0 +1,66 @@
+import discord
+import asyncio
+import os
+from discord.ext import commands
+import urllib
+from urllib.request import URLError
+from urllib.request import HTTPError
+from urllib.request import urlopen
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+import re
+import warnings
+import requests
+import unicodedata
+import json
+
+
+client_id = ""
+client_secret = ""
+
+class Translate(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Commands
+    @commands.command()
+    async def 한영(self, ctx):
+        reply = ctx.message.content.split(" ")
+        if len(reply) > 1:
+            for i in range(2, len(reply)):
+                reply[1] = reply[1] + " " + reply[i]
+
+        baseurl = "https://openapi.naver.com/v1/papago/n2mt"
+        try:
+            if len(reply) == 1:
+                await ctx.send("단어 혹은 문장이 입력되지 않았어요. 다시한번 확인해주세요.")
+            else:
+                dataParmas = "source=ko&target=en&text=" + reply[1]
+                # Make a Request Instance
+                request = Request(baseurl)
+                # add header to packet
+                request.add_header("X-Naver-Client-Id", client_id)
+                request.add_header("X-Naver-Client-Secret", client_secret)
+                response = urlopen(request, data=dataParmas.encode("utf-8"))
+
+                responsedCode = response.getcode()
+                if (responsedCode == 200):
+                    response_body = response.read()
+                    # response_body -> byte string : decode to utf-8
+                    api_callResult = response_body.decode('utf-8')
+                    # JSON data will be printed as string type. So need to make it back to type JSON(like dictionary)
+                    api_callResult = json.loads(api_callResult)
+                    # Final Result
+                    translatedText = api_callResult['message']['result']["translatedText"]
+                    embed = discord.Embed(title="번역결과", description=translatedText, color=0xffffff)
+                    embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio.png")
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send("에러 코드: " + responsedCode)
+        except HTTPError as e:
+            await ctx.send("오류가 발생하여 번역에 실패했어요.")
+
+def setup(client):
+    client.add_cog(Translate(client))
