259475f41f8f402030094fd54458169415a0dceb
SaltCube <shio7113@gmail.com>
2020-05-27 13:27:04 +0900

    help

--- cogs/help.py
+++ cogs/help.py
@@ -15,6 +15,8 @@ class Help(commands.Cog):
         embed.add_field(name='**도움**', value='**이 메시지를 보여줍니다.**', inline = False)
         embed.add_field(name='**아바타**', value='**본인의 아바타 또는 멘션된 유저의 아바타를 보여줍니다.**', inline = False)
         embed.add_field(name='**말하기**', value='**봇이 말을하게 만들 수 있습니다.**', inline = False)
+        embed.add_field(name='**핑**', value='**봇의 지연시간을 확인합니다**', inline = False)
+        embed.add_field(name='**한영**', value='**한국어를 영어로 번역해줍니다**', inline = False)
         embed.add_field(name='**지원 서버**', value='**[클릭!](https://discord.gg/HerTmj5)**', inline = False)
         embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio.png")
         await ctx.send(embed=embed)
