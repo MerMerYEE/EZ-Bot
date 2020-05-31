0db686dc9182985104d099ddba23891888087d6d
SaltCube <shio7113@gmail.com>
2020-05-31 13:52:07 +0900

    remove quotattion

--- cogs/en_avatar.py
+++ cogs/en_avatar.py
@@ -12,7 +12,7 @@ class en_Avatar(commands.Cog):
         if (ctx.message.mentions.__len__() > 0):
             for user in ctx.message.mentions:
                 pfp = str(user.avatar_url)
-                embed = discord.Embed(title="**" +user.name + "**'s Avatar'", description="[Link]" + "(" + pfp + ")",
+                embed = discord.Embed(title="**" +user.name + "**'s Avatar", description="[Link]" + "(" + pfp + ")",
                                       color=0xffffff)
                 embed.set_image(url=pfp)
                 embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio7.png")
@@ -20,7 +20,7 @@ class en_Avatar(commands.Cog):
                 await ctx.send(embed=embed)
         else:
             pfp = ctx.author.avatar_url
-            embed = discord.Embed(title="**" + ctx.author.name + "**'s Avatar'", description="[Link]" + "(" + str(pfp) + ")",
+            embed = discord.Embed(title="**" + ctx.author.name + "**'s Avatar", description="[Link]" + "(" + str(pfp) + ")",
                                 color=0xffffff)
             embed.set_image(url=pfp)
             embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio7.png")
