c33e9b1627d7f9140d7eadf733d6ccab458c35af
SaltCube <shio7113@gmail.com>
2020-05-31 17:06:54 +0900

    en ver

---
+++ cogs/en_calculate.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import wolframalpha
+
+class en_Calculate(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Commands
+    @commands.command()
+    async def calculate(self, ctx):
+        await ctx.trigger_typing()
+        reply = ctx.message.content.split(" ")
+        if len(reply) > 1:
+            for i in range(2, len(reply)):
+                reply[1] = reply[1] + " " + reply[i]
+        app_id = "VPR9G7-54PV53JYTK"
+        client = wolframalpha.Client(app_id)
+        res = client.query(reply[1])
+        answer = next(res.results).text
+        embed = discord.Embed(title="Result", description=answer, color=0xffffff)
+        embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio7.png")
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(en_Calculate(client))
