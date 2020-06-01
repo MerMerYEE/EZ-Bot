cc386f9c8d8d9e4fe370fd88f09d695ca08e8d87
SaltCube <shio7113@gmail.com>
2020-06-01 16:30:42 +0900

    ko_help

--- cogs/ko_help.py
+++ cogs/ko_help.py
@@ -11,7 +11,7 @@ class ko_Help(commands.Cog):
     @commands.command()
     async def 도움(self, ctx):
         await ctx.trigger_typing()
-        embed = discord.Embed(title="도움말", description= "**항상 이지을 사랑해주시고 이용해주셔서 감사합니다.\n이 봇은 `Shio#7951`와 함께 개발된\n [오픈소스](https://github.com/Shio7/EZ-Bot) 프로젝트입니다.**", color=0x8680df)
+        embed = discord.Embed(title="도움말", description= "**항상 이지봇을 사랑해주시고 이용해주셔서 감사합니다.\n이 봇은 `Shio#7951`와 함께 개발된\n [오픈소스](https://github.com/Shio7/EZ-Bot) 프로젝트입니다.**", color=0x8680df)
         embed.add_field(name='**도움**', value='**이 메시지를 보여줍니다.**', inline = False)
         embed.add_field(name='**Utility**', value='**`아바타`, `아바타 (멘션)`, `말하기`, `핑`, `계산 (식)`, `주사위`, `한영`, `영한`, `한일`, `일한`, `한중`, `중한`**', inline = False)
         embed.add_field(name='**서버 이벤트**', value='**`포인트`, `포인트 (멘션)`, `포인트추가 (멘션)`**', inline = False)
