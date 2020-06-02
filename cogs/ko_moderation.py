4e5130e8fa4479d9ad3bbdd7497af66ef1cacbf4
SaltCube <shio7113@gmail.com>
2020-06-02 16:26:18 +0900

    Update ko_moderation.py

--- cogs/ko_moderation.py
+++ cogs/ko_moderation.py
@@ -17,6 +17,20 @@ class ko_Moderation(commands.Cog):
     @commands.command()
     @has_permissions(administrator = True)
     async def 뮤트(self, ctx):
+        if (ctx.message.mentions.__len__() > 0):
+            for user in ctx.message.mentions:
+                await ctx.trigger_typing()
+                embed = discord.Embed(title="**테스트중**", description= "당신은 어드민입니다.", color=0x8680df)
+                embed.set_thumbnail(url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/shio_error.png")
+                embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio8.png")
+                await ctx.send(embed=embed)
+        else:
+            await ctx.trigger_typing()
+            embed = discord.Embed(title="**에러!**", description= "음소거할 대상을 멘션해주세요", color=0x8680df)
+            embed.set_thumbnail(url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/shio_error.png")
+            embed.set_footer(text="Offered by NACL - Shio", icon_url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/Shio8.png")
+            await ctx.send(embed=embed)
+
         await ctx.trigger_typing()
         embed = discord.Embed(title="**테스트중**", description= "당신은 어드민입니다.", color=0x8680df)
         embed.set_thumbnail(url="https://raw.githubusercontent.com/Shio7/EZ-Bot/master/images/shio_error.png")
